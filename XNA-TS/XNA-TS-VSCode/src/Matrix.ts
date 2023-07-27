bd898a29378353921b343f2ce6fb9b7ac49fabc5
Ian Wigley <ian.wigley@outlook.com>
2023-07-27 18:18:56 +0100

    Create Matrix.ts

---
+++ XNA-TS/XNA-TS-VSCode/src/Matrix.ts
@@ -0,0 +1,21 @@
+﻿class Matrix {
+
+    public M11: number;
+    public M12: number;
+    public M13: number;
+    public M14: number;
+    public M21: number;
+    public M22: number;
+    public M23: number;
+    public M24: number;
+    public M31: number;
+    public M32: number;
+    public M33: number;
+    public M34: number;
+    public M41: number;
+    public M42: number;
+    public M43: number;
+    public M44: number;
+
+
+}
